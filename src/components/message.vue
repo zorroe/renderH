63e6d4c48fe2f442d18f27c5a95ff7542fad002c
lixuan <1318753541@qq.com>
2024-01-16 10:15:15 +0800

    优化出现及消失动画

--- src/components/message.vue
+++ src/components/message.vue
@@ -2,13 +2,14 @@
   <Transition
     enter-active-class="animate__animated meslideInDown"
     leave-active-class="animate__animated meslideOutUp"
-  >
+    mode="out-in">
     <div
-      class="rounded border-s-4 px-4 py-2"
+      class="rounded border-s-4 px-4 py-2 my-2 transition-all duration-100 ease-in-out"
       :class="'bg-' + props.type"
-      v-if="visible"
-    >
-      <p class="text-sm" :class="'msg-' + props.type">
+      v-if="visible">
+      <p
+        class="text-sm"
+        :class="'msg-' + props.type">
         {{ props.message }}
       </p>
     </div>
@@ -16,33 +17,33 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
-import { removeInstance } from "./index";
+import { onMounted, ref } from 'vue'
+import { removeInstance } from './index'
 
 interface Props {
-  message?: string;
-  type?: string;
-  duration?: number;
-  id?: string;
+  message?: string
+  type?: string
+  duration?: number
+  id?: string
 }
 
-const visible = ref(false);
+const visible = ref(false)
 
 const props = withDefaults(defineProps<Props>(), {
-  message: "",
-  type: "success",
+  message: '',
+  type: 'success',
   duration: 4500,
-});
+})
 
 onMounted(() => {
-  visible.value = true;
+  visible.value = true
   setTimeout(() => {
-    visible.value = false;
-  }, props.duration);
+    visible.value = false
+  }, props.duration)
   setTimeout(() => {
-    removeInstance(props.id);
-  }, props.duration + 600);
-});
+    removeInstance(props.id)
+  }, props.duration + 1000)
+})
 </script>
 <style>
 .bg-success {
@@ -90,6 +91,7 @@ onMounted(() => {
 
 .meslideInDown {
   animation-name: slideInDown;
+  animation-duration: 200ms;
 }
 
 @keyframes slideOutUp {
@@ -99,11 +101,14 @@ onMounted(() => {
 
   to {
     opacity: 0;
-    transform: translate3d(0, -100%, 0);
+    height: 0;
+    margin: 0;
+    padding: 0;
   }
 }
 
 .meslideOutUp {
   animation-name: slideOutUp;
+  animation-duration: 200ms;
 }
 </style>
